ac032bda219ca757482d7acb87ea4d03cdadd614
Jamie R. Oaks <joaks1@gmail.com>
2019-03-21 15:55:35 -0500

    Working on shell script for making animations.
    Using a hack to get a pause at the end of animation with ffmpeg.

--- create-animated-map.sh
+++ create-animated-map.sh
@@ -6,6 +6,7 @@ frames_per_second=10
 delay=$(expr 100 / $frames_per_second) # 100 = 1 second
 size=1048576
 final_pause=300 # 100 = 1 second
+num_extra_frames_for_pause=30
 
 ./plot-bathymetry.R
 
@@ -29,6 +30,21 @@ convert -monitor \
 #         frame-????-????kybp-bathy-small.png \
 #         -delay "$final_pause" frame-????-0000kybp-bathy-small.png \
 #         animated-bathymetry-im.mp4
+
+# Setting a prolonged pause on the last frame in ffmpeg is too convoluted for
+# my simple mind, so I'm doing a hack to simply repeat the last frame
+last_frame_path="$(ls frame-????-????kybp-bathy-small.png | tail -n 1)"
+last_frame_index_str="${last_frame_path/frame-/}"
+last_frame_index_str="${last_frame_index_str/-0000kybp-bathy-small.png/}"
+last_frame_index="$(expr $last_frame_index_str + 1)"
+end_frame_index="$(expr $last_frame_index + $num_extra_frames_for_pause - 1)"
+for i in $(seq $last_frame_index $end_frame_index)
+do
+    frame_label="$(printf "%0${#last_frame_index_str}d" $i)"
+    next_frame_path="${last_frame_path/$last_frame_index_str/$frame_label}"
+    cp "$last_frame_path" "$next_frame_path"
+done
+
 # ffmpeg -f image2 \
 #         -framerate "$frames_per_second" \
 #         -pattern_type glob \
@@ -44,13 +60,6 @@ ffmpeg -f image2 \
         -crf 18 \
         -pix_fmt yuv420p \
         animated-bathymetry.mp4
-# ffmpeg -i 'animated-bathymetry.gif' \
-#         -vf 'scale=trunc(iw/2)*2:trunc(ih/2)*2' \
-#         -movflags faststart \
-#         -c:v libx264 -profile:v high \
-#         -crf 18 \
-#         -pix_fmt yuv420p \
-#         animated-bathymetry-from-gif.mp4
 
 rm frame-????-????kybp-bathy.png
 rm frame-????-????kybp-bathy-small.png
